ca45a0f1dcbff1b06aa6fc3c9f87cbcfa10aec61
Jhonny <jhonnycgarcia@gmail.com>
2024-10-16 11:06:03 -0500

    Primeros pasos formularios reactivos.

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+
+  public myForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      price: [0, Validators.required],
+      inStorage: [0, Validators.required]
+    });
+  }
+
+  onSave(): void {
+    console.log({form: this.myForm.value});
+  }
 
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -5,6 +5,7 @@ import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ReactiveRoutingModule
   ]
 })
