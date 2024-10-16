ad850ee6ed0f6463c846dd4f34caa39c81e36795
Jhonny <jhonnycgarcia@gmail.com>
2024-10-16 14:27:49 -0500

    Validator Service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { canBeStrider, emailPattern, firstNameAndLastnamePattern } from '../../../shared/validators/validators.helpers';
+import { ValidatorsService } from '../../../shared/services/validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -13,30 +13,34 @@ export class RegisterPageComponent {
       '',
       [
         Validators.required,
-        Validators.pattern(firstNameAndLastnamePattern)
+        Validators.pattern(this.validatorSrv.firstNameAndLastnamePattern)
       ]
     ],
     email: [
       '',
       [
         Validators.required,
-        Validators.pattern(emailPattern)
+        Validators.pattern(this.validatorSrv.emailPattern)
       ]
     ],
     username: [
       '',
-      [ Validators.required, canBeStrider ]
+      [
+        Validators.required,
+        this.validatorSrv.canBeStrider
+      ]
     ],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', Validators.required],
   });
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private validatorSrv: ValidatorsService
   ) { }
 
-  isValidField(field: string): void {
-    // TODO: obtener validación desde un servicio
+  isValidField(field: string): boolean | null {
+    return this.validatorSrv.isValidField(this.myForm, field);
   }
 
   onSubmit(): void {
---
+++ src/app/shared/services/validators.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { FormControl, ValidationErrors, FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidatorsService {
+
+  public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  public canBeStrider(control: FormControl): ValidationErrors | null {
+    const value: string = control.value.trim().toLowerCase();
+    return (value === 'strider') ? { noStrider: true} : null;
+  }
+
+  public isValidField(form: FormGroup, field: string): boolean | null {
+    return form.controls[field].errors
+      && form.controls[field].touched;
+  }
+
+}
