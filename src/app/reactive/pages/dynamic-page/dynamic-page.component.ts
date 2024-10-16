d7eaae48696e7530be4540442bca2d41e244cd20
Jhonny <jhonnycgarcia@gmail.com>
2024-10-16 12:55:52 -0500

    Formularios dinámicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
 export class DynamicPageComponent {
+
+  public myForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      favoriteGames: this.fb.array([
+        ['Metal Gear', Validators.required],
+        ['Resident Evil', Validators.required],
+      ]),
+    });
+  }
+
+  get favoriteGames(): FormArray {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit(): void {
+    if(!this.myForm.valid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 
 }
