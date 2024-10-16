1dcb1d3bba9016be8934cfd3eccda722b3c4a2c2
Jhonny <jhonnycgarcia@gmail.com>
2024-10-16 13:37:18 -0500

    Formulario de Registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', Validators.required],
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  isValidField(field: string): void {
+    // TODO: obtener validación desde un servicio
+  }
+
+  onSubmit(): void {
+    if(!this.myForm.valid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+  }
 
 }
